a78bb0d5780d5aa706f2b38d1ee30daa631bfaca
rkhalil38 <romuluskhalil3@gmail.com>
2023-11-11 17:08:51 -0500

    Syncing with azure

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar'
 export default async function Home() {
 
   const api_endpoint = "http://localhost:3000/api/searchbar"
+  console.log(api_endpoint)
 
   return (
     <main className='flex w-screen h-screen'>
