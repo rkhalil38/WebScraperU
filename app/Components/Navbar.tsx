4e33de6fa26ab31fa9326d0f99c4324e52d3a732
rkhalil38 <romuluskhalil3@gmail.com>
2023-11-11 21:48:18 -0500

    redirected users to webscraper page instead of personal

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     
     return (
         <div className='container flex bg-orange-400 flex-row items-center justify-center absolute w-24 h-12 rounded-lg mx-3 my-3'>
-            <Link href='https://github.com/rkhalil38' rel="noopener noreferrer" target="_blank" className='text-white px-[5px] py-1 w-10 h-10 rounded-lg hover:bg-orange-500 text-3xl cursor-pointer'><AiFillGithub/></Link>
+            <Link href='https://github.com/rkhalil38/WebScraperU' rel="noopener noreferrer" target="_blank" className='text-white px-[5px] py-1 w-10 h-10 rounded-lg hover:bg-orange-500 text-3xl cursor-pointer'><AiFillGithub/></Link>
             <Link href='https://www.linkedin.com/in/romulus-khalil/' rel="noopener noreferrer" target="_blank" className='text-white px-2 py-2 w-10 h-10 rounded-lg hover:bg-orange-500 text-2xl cursor-pointer'><BsLinkedin/></Link>
         </div>
     )
