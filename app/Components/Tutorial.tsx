d80b65b6a675bbe2cba2152e8b83ab59697ec70a
rkhalil38 <romuluskhalil3@gmail.com>
2023-11-13 22:34:47 -0500

    testing if the eslint error is present without image

--- app/Components/Tutorial.tsx
+++ app/Components/Tutorial.tsx
@@ -19,18 +19,12 @@ const Tutorial = () => {
         <div className="container flex w-full">
             
             { isActive? 
-                <div className='flex flex-row bg-orange-500 w-full h-80 rounded-lg'>
+                <div className="flex flex-row bg-orange-500 w-full h-80 rounded-lg">
                     <div className="w-1/12 h-full rounded-l-lg"></div>
                     <div className="w-10/12 h-full">
                         <h1 className="text-white text-2xl font-semibold text-center mt-2 mb-1">Welcome to WebScraperU!</h1>
                         <p className="text-white font-semibold text-center mb-2">Here's a quick example on how to use your own webscraper.</p>
-                        <Image className="rounded-lg"
-                            src={tutorialPic}
-                            priority={true}
-                            alt="tutorial"
-                            height={500}
-                            width={900}
-                        />
+                        
                     </div>
                     <div className="w-1/12 text-right rounded-r-lg">
                         <button onClick={ handleToggle } className="text-white hover:text-red-600 px-3 py-3 text-3xl"><BsXLg/></button>
