724db0897c3ed6ef5912d4d11174bee212774038
rkhalil38 <romuluskhalil3@gmail.com>
2023-12-22 23:58:15 -0500

    added open graph support

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,19 @@ import { Analytics } from '@vercel/analytics/react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'WebScraperU',
+  title: 'WebScraperU | Web Scrape for Free',
   description: 'A personalized WebScraper that allows anyone to look for specific words or phrases on any website.',
   keywords: ['Web Scraper', 'Word Finder', 'Sentence Finder', 'Web Scraper U', 'WebScraperU', 'scrape web', 'WebScraperU.com', 'web scrape'],
   authors: [{name: 'Romulus Khalil'}],
   creator: 'Romulus Khalil',
+  openGraph: {
+    title: 'WebScraperU | Web Scrape for Free',
+    description: 'A personalized WebScraper that allows anyone to look for specific words or phrases on any website.',
+    url: 'https://www.webscraperu.com',
+    type: 'website',
+    siteName: 'WebScraperU',
+    images: '/og-image.png',
+  }
 }
 
 export default function RootLayout({
